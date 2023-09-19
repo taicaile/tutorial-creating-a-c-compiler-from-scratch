c394c5ca1253e148e506749e9320ef855e67d3ea
taicaile <23215696+taicaile@users.noreply.github.com>
2023-09-19 21:34:12 +0800

    unit 6 done

--- compiler.h
+++ compiler.h
@@ -1,8 +1,49 @@
 #ifndef __COMPILER_H__
 #define __COMPILER_H__
 #include <stdio.h>
+#include <stdbool.h>
 
-enum{
+struct pos
+{
+    int line;
+    int col;
+    const char *filename;
+};
+
+enum
+{
+    TOKEN_TYPE_IDENTIFIER,
+    TOKEN_TYPE_KEYWORD,
+    TOKEN_TYPE_OPERATOR,
+    TOKEN_TYPE_SYMBOL,
+    TOKEN_TYPE_NUMBER,
+    TOKEN_TYPE_COMMENT,
+    TOKEN_TYPE_NEWLINE
+};
+
+struct token
+{
+    int type;
+    int flags;
+
+    union
+    {
+        char cval;
+        const char *sval;
+        unsigned int inum;
+        unsigned long lnum;
+        unsigned long long llnum;
+        void *any;
+    };
+
+    // True if there is whitespace between the token and the next token.
+    bool whitespace;
+
+    const char *between_brackets;
+};
+
+enum
+{
     COMPILER_FILE_COMPILED_OK,
     COMPILER_FAILED_WITH_ERRORS
 };
